159b7d51e2995837ae1f503df9380d03e75dc37c
Salman <salmanfaris98@gmail.com>
2025-11-14 21:15:00 +0000

    baby steps: finally got the time embeddings working with correct shapes

--- nanodlm/model.py
+++ nanodlm/model.py
@@ -8,8 +8,8 @@ from nanodlm.dataset import CharacterLevelDataset
 
 
 @dataclass
-class GPTConfig:
-    """Configuration for GPT model."""
+class TransformerConfig:
+    """Base configuration for Transformer models."""
 
     smol: bool = True
 
@@ -24,6 +24,24 @@ class GPTConfig:
     is_causal: bool = True
 
 
+@dataclass
+class GPTConfig(TransformerConfig):
+    """Configuration for GPT model."""
+
+    pass
+
+
+@dataclass
+class DLMConfig(TransformerConfig):
+    """Configuration for NanoDiffusionLM model."""
+
+    smol: bool = True
+
+    is_causal: bool = False
+
+    diffusion_steps: int = 1000 if not smol else 100
+
+
 class Buffer(nnx.Variable):
     pass
 
@@ -31,7 +49,7 @@ class Buffer(nnx.Variable):
 class Head(nnx.Module):
     """Single head of self-attention."""
 
-    def __init__(self, config: GPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: TransformerConfig, rngs: nnx.Rngs):
         self.is_causal = config.is_causal
 
         head_size = config.n_embd // config.n_head
@@ -72,7 +90,7 @@ class Head(nnx.Module):
 class MultiHeadAttention(nnx.Module):
     """Mutli-head self-attention."""
 
-    def __init__(self, config: GPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: TransformerConfig, rngs: nnx.Rngs):
         self.heads = nnx.Sequential(
             *[Head(config=config, rngs=rngs) for _ in range(config.n_head)]
         )
@@ -93,7 +111,7 @@ class MultiHeadAttention(nnx.Module):
 class FeedForward(nnx.Module):
     """A simple linear layer followed by a non-linearity."""
 
-    def __init__(self, config: GPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: TransformerConfig, rngs: nnx.Rngs):
         self.net = nnx.Sequential(
             nnx.Linear(config.n_embd, 4 * config.n_embd, rngs=rngs),
             nnx.relu,
@@ -110,7 +128,7 @@ class FeedForward(nnx.Module):
 class Block(nnx.Module):
     """Transformer block: communication followed by computation."""
 
-    def __init__(self, config: GPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: TransformerConfig, rngs: nnx.Rngs):
         self.sa = MultiHeadAttention(config=config, rngs=rngs)
         self.ffwd = FeedForward(config=config, rngs=rngs)
         self.ln1 = nnx.LayerNorm(config.n_embd, rngs=rngs)
@@ -123,7 +141,7 @@ class Block(nnx.Module):
 
 
 class GPT(nnx.Module):
-    def __init__(self, config: GPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: TransformerConfig, rngs: nnx.Rngs):
         self.block_size = config.block_size  # Use for generation.
 
         assert config.vocab_size is not None
@@ -262,3 +280,92 @@ class GPT(nnx.Module):
         )
 
         return final_tokens
+
+
+class NanoDiffusionLM(nnx.Module):
+    def __init__(self, config: DLMConfig, rngs: nnx.Rngs):
+        self.block_size = config.block_size  # Use for generation.
+
+        assert config.vocab_size is not None
+
+        self.token_embedding_table = nnx.Embed(
+            num_embeddings=config.vocab_size, features=config.n_embd, rngs=rngs
+        )
+        self.positional_embedding_table = nnx.Embed(
+            num_embeddings=config.block_size, features=config.n_embd, rngs=rngs
+        )
+        self.time_embedding_table = nnx.Embed(
+            num_embeddings=config.diffusion_steps, features=config.n_embd, rngs=rngs
+        )
+        self.blocks = nnx.Sequential(
+            *[Block(config=config, rngs=rngs) for _ in range(config.n_layer)]
+        )
+        self.ln_f = nnx.LayerNorm(config.n_embd, rngs=rngs)  # Final layer norm
+        self.lm_head = nnx.Linear(config.n_embd, config.vocab_size, rngs=rngs)
+
+    def __call__(self, idx: jnp.ndarray, time_idx: jnp.ndarray) -> jnp.ndarray:
+        _, T = idx.shape
+
+        # Think of logits as scores for the next char in the sequence.
+        tok_emb = self.token_embedding_table(idx)  # (Batch, Time, Channel) = (B, T, C)
+        pos_emb = self.positional_embedding_table(jnp.arange(T))  # (T, C)
+
+        # Add time embeddings
+        # time_idx.shape = (B,)
+        time_emb = self.time_embedding_table(time_idx)  # (B, C)
+
+        # (B, T, C) + (T, C) --> (B, T, C) + (1, T, C) = (B, T, C).
+        # Note: not adding dimensions here, but we are showing how jax infers the batch
+        # dimension in `pos_embd` and right-shift (T, C) -> (1, T, C) similar to in
+        # pytorch.
+        x = tok_emb + pos_emb
+
+        # Broadcast time embedding over char dim T.
+        # (B, T, C) + (B, C) -> (B, T, C) + (B, 1, C) -> (B, T, C)
+        # Note: we are not adding dimensions here.
+        x = x + time_emb[:, None, :]
+
+        x = self.blocks(x)  # (B, T, C)
+        x = self.ln_f(x)  # (B, T, C)
+        logits = self.lm_head(x)  # (B, T, VOCAB_SIZE)
+
+        return logits
+
+    # @nnx.jit
+    # def generate_step(self, padded_tokens, sample_index, rngs):
+    #     logits = self(padded_tokens)
+
+    #     # logits = logits[:, sample_index, :].squeeze(0)  # this is not jit-able
+    #     logits = logits[0][sample_index]
+
+    #     # sample_from equiv.
+    #     next_token = rngs.categorical(logits)
+
+    #     return next_token
+
+    # def generate_text(
+    #     self,
+    #     dataset: CharacterLevelDataset,
+    #     max_tokens: int,
+    #     start_tokens: list,
+    #     rngs,
+    # ):
+    #     generated = []
+
+    #     print(dataset.decode(start_tokens), flush=True, end="")
+    #     for _ in range(max_tokens):
+    #         sample_index = len(start_tokens) + len(generated) - 1
+
+    #         padded_tokens = (
+    #             start_tokens
+    #             + generated
+    #             + [0] * (self.block_size - len(start_tokens) - len(generated))
+    #         )
+    #         padded_tokens = padded_tokens[-self.block_size :]  # Truncate to block size
+    #         padded_tokens = jnp.array(padded_tokens).reshape(1, -1)
+
+    #         next_token = int(self.generate_step(padded_tokens, sample_index, rngs))  # type: ignore
+    #         generated.append(next_token)
+    #         print(dataset.decode([next_token]), flush=True, end="")
+
+    #     return dataset.decode(start_tokens + generated)
--- train_diffusion.py
+++ train_diffusion.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from dataclasses import dataclass
-from typing import Literal
+from typing import Literal, cast
 
 import jax
 import jax.numpy as jnp
@@ -11,7 +11,7 @@ from flax import nnx
 
 from nanodlm.dataset import load_shakespeare_dataset
 from nanodlm.loader import save_checkpoint, set_ckpt_dir
-from nanodlm.model import GPT, GPTConfig
+from nanodlm.model import DLMConfig, NanoDiffusionLM
 from nanodlm.utils import log_model_size, log_system_info, setup_logging
 
 # Setup logging
@@ -21,6 +21,8 @@ logger = logging.getLogger(__name__)
 ckpt_dir = set_ckpt_dir()
 
 log_system_info()
+
+TrainModel = NanoDiffusionLM
 
 
 @dataclass
@@ -39,110 +41,49 @@ class TrainConfig:
 rngs = nnx.Rngs(44)
 
 smol = True
-gpt_config = GPTConfig(smol=smol)
+dlm_config = DLMConfig(smol=smol)
 train_config = TrainConfig(smol=smol)
 
 # Load dataset
 dataset = load_shakespeare_dataset(train_split=0.9)
-gpt_config.vocab_size = dataset.vocab_size
-train_data = dataset.train_data
-val_data = dataset.val_data
+dlm_config.vocab_size = dataset.vocab_size
 
 logger.info("--" * 12)
-logger.info(f"GPT Config: {gpt_config}")
+logger.info(f"DLM Config: {dlm_config}")
 logger.info(f"Train Config: {train_config}")
 logger.info("--" * 12)
-
-
-def get_batch_not_jittable(
-    rngs: nnx.Rngs,
-    split: Literal["train", "val"],
-    block_size: int,
-    batch_size: int,
-):
-    """Shakespeare data loader that mimics Karpathy's pytorch implementation."""
-    # Generate a small batch of data of inputs x and targets y
-    data = train_data if split == "train" else val_data
-
-    maxval = len(data) - block_size
-    start_indices = rngs.randint(shape=(batch_size,), minval=0, maxval=maxval)
-
-    x = jnp.stack([data[i : i + block_size] for i in start_indices])
-    y = jnp.stack([data[i + 1 : i + 1 + block_size] for i in start_indices])
-
-    return x, y
-
-
-@nnx.jit(static_argnums=(2, 3, 4))
-def get_batch(
-    rngs: nnx.Rngs,
-    data: jnp.ndarray,
-    num_samples: int,
-    batch_size: int,
-    block_size: int,
-) -> tuple[jnp.ndarray, jnp.ndarray]:
-    """Shakespeare data loader that is JIT-compilable.
-
-    Pre-generate all random indices for the batch based on num_samples, then use
-    jax.vmap to extract sequences from the data array.
-    """
-    maxval = len(data) - block_size
-    # Generate indices with dim = (num_samples, batch_size).
-    all_indices = rngs.randint(shape=(num_samples, batch_size), minval=0, maxval=maxval)
-
-    def extract_sequence(start_indices: jnp.ndarray) -> tuple[jnp.ndarray, jnp.ndarray]:
-        """Extract sequences for a batch of start_indices.
-
-        Don't let the jax.vmap and lax.dynamic_slice scare you!
-        lax.dynamic_slice is just a way to do slicing with static shapes that JAX
-        can reason about during JIT compilation. jax.vmap is just a way to vectorize
-        operations over a batch dimension.
-
-        This:
-            x = jax.vmap(lambda idx: jax.lax.dynamic_slice(data, (idx,), (block_size,)))(
-                start_indices
-            )
-        is equivalent to the non-jitable code below:
-            x = jnp.stack([data[i : i + block_size] for i in start_indices])
-        """
-        x = jax.vmap(lambda idx: jax.lax.dynamic_slice(data, (idx,), (block_size,)))(
-            start_indices
-        )
-        y = jax.vmap(
-            lambda idx: jax.lax.dynamic_slice(data, (idx + 1,), (block_size,))
-        )(start_indices)
-        return x, y
-
-    x, y = jax.vmap(extract_sequence)(all_indices)
-    return x, y
 
 
 # We don't jit `loss_fn` because we will be using it within the training (train_step)
 # and eval step (estimate_loss). When jit compiles the training/eval step, it traces
 # through all operations inside the function, which in this case includes `loss_fn`.
 # This reduces trace complexity.
-def loss_fn(model, idx, targets):
-    logits = model(idx)
+def loss_fn(model: TrainModel, idx, targets, timesteps):
+    logits = model(idx, timesteps)
 
     B, T, C = logits.shape
     logits = logits.reshape(B * T, C)
     targets = targets.reshape(B * T)
-    loss = jnp.mean(
-        # Can verify softmax using the average ~= -log(1/VOCAB_SIZE) = 4.17.
-        optax.softmax_cross_entropy_with_integer_labels(logits, targets)
-    )
+    loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logits, targets))
     return loss, logits
 
 
 @nnx.jit
-def train_step(model, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, idx, targets):
+def train_step(
+    model, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, idx, targets, rngs
+):
     """Train for a single step."""
+    B, _ = idx.shape
+
+    # Sample random time steps for diffusion of dim (batch_size,)
+    tb = rngs.randint(shape=(B,), minval=0, maxval=dlm_config.diffusion_steps)
+
     grad_fn = nnx.value_and_grad(loss_fn, has_aux=True)
     # Notice we don't require a "zero grad" operation like in pytorch where we would
     # done something like `optimizer.zero_grad()` because JAX doesn't have gradient
     # accumulation by default like pytorch. Instead, gradients are fresh per function
     # call.
-    (loss, logits), grads = grad_fn(model, idx, targets)
+    (loss, logits), grads = grad_fn(model, idx, targets, tb)
     metrics.update(loss=loss, logits=logits)
     optimizer.update(model, grads)
 
@@ -151,31 +92,35 @@ def train_step(model, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, idx, t
 def estimate_loss(rngs: nnx.Rngs, model):
     def eval_step(_, step_data: jnp.ndarray) -> tuple[None, jnp.ndarray]:
         """Single eval step."""
-        x, y = step_data
-        loss, _ = loss_fn(model, x, y)
+        x, y, t = step_data
+        loss, _ = loss_fn(model, x, y, t)
         return None, loss
 
     out = {}
     model.eval()
 
-    for split, data in [("train", train_data), ("val", val_data)]:
-        x, y = get_batch(
-            rngs,
-            data,
-            train_config.eval_iters,
-            train_config.batch_size,
-            gpt_config.block_size,
+    for split in ["train", "val"]:
+        x, y = dataset.get_batch_jit(
+            rngs=rngs,
+            split=cast(Literal["train", "val"], split),
+            num_samples=train_config.eval_iters,
+            batch_size=train_config.batch_size,
+            block_size=dlm_config.block_size,
         )
+        B, _, _ = x.shape
+        # For jax.lax.scan scanning t together with x, y with dim (B, ...), we need t
+        # to have shape (B, 1) not (B,). Otherwise, it will not scan as expected.
+        t = rngs.randint(shape=(B, 1), minval=0, maxval=dlm_config.diffusion_steps)
 
         # Scan over pre-generated indices, this pattern enables jit!
-        _, losses = jax.lax.scan(eval_step, None, (x, y))  # type: ignore
+        _, losses = jax.lax.scan(eval_step, None, (x, y, t))  # type: ignore
         out[split] = losses.mean()
 
     model.train()
     return out
 
 
-model = GPT(gpt_config, rngs=rngs)
+model = NanoDiffusionLM(dlm_config, rngs=rngs)
 log_model_size(model)
 
 # Train the function
@@ -188,7 +133,18 @@ metrics = nnx.MultiMetric(loss=nnx.metrics.Average("loss"))
 training_start_time = time.perf_counter()
 
 for iters in range(train_config.max_iters):
-    model.train()
+    xb, yb = dataset.get_batch_jit(
+        rngs=rngs,
+        split="train",
+        num_samples=1,
+        batch_size=train_config.batch_size,
+        block_size=dlm_config.block_size,
+    )
+    xb = jnp.squeeze(xb, axis=0)
+    yb = jnp.squeeze(yb, axis=0)
+
+    # Evaluate the loss
+    train_step(model, optimizer, metrics, xb, yb, rngs=rngs)
 
     # Every once in a while evaluate the loss on train and val sets
     if iters % train_config.eval_interval == 0:
@@ -201,35 +157,23 @@ for iters in range(train_config.max_iters):
         # Save checkpoint
         save_checkpoint(ckpt_dir, iters, model, optimizer)
 
-    # Sample a batch of data
-    xb, yb = get_batch(
-        rngs,
-        train_data,
-        num_samples=1,
-        batch_size=train_config.batch_size,
-        block_size=gpt_config.block_size,
-    )
-    xb = jnp.squeeze(xb, axis=0)
-    yb = jnp.squeeze(yb, axis=0)
-
-    # Evaluate the loss
-    train_step(model, optimizer, metrics, xb, yb)
+        model.train()
 
 # Generate from the model
 model.eval()
 
-context = jnp.zeros((1, 1), dtype=jnp.int32)
-logger.info("Generating from trained model:")
-print("--" * 20)
-print(
-    model.generate_text(
-        dataset,
-        max_tokens=500,
-        start_tokens=context[0].tolist(),
-        rngs=rngs,
-    )
-)
-print("--" * 20)
+# context = jnp.zeros((1, 1), dtype=jnp.int32)
+# logger.info("Generating from trained model:")
+# print("--" * 20)
+# print(
+#     model.generate_text(
+#         dataset,
+#         max_tokens=500,
+#         start_tokens=context[0].tolist(),
+#         rngs=rngs,
+#     )
+# )
+# print("--" * 20)
 
 # Total time elapsed
 time_elapsed = time.perf_counter() - training_start_time
